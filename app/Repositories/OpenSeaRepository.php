d9f7233099b99173cb33e1bda2b2f55bf61b0319
Puppyter <sokoldenys00@gmail.com>
2022-07-06 02:19:39 +0300

    ready to host

--- app/Repositories/OpenSeaRepository.php
+++ app/Repositories/OpenSeaRepository.php
@@ -65,8 +65,13 @@ class OpenSeaRepository
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
         $data = curl_exec($curl);
-
+        if (curl_getinfo($curl,CURLINFO_HTTP_CODE)!= 200)
+        {
+            curl_close($curl);
+            return false;
+        }
         curl_close($curl);
+
         $html = str_get_html($data);
 
 
